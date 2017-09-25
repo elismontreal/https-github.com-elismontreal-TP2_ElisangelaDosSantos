dd985054ce07ff4d5ceba935ec57ca4206b74434
Elisangela Santos <elis@MacBook-Pro-de-Elisangela.local>
2017-09-25 09:18:26 -0400

    Initial Commit

---
+++ memoAnime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memoAnime
+//
+//  Created by Elisangela dos Santos on 17-09-25.
+//  Copyright © 2017 Elisangela dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
